ad9b38c46988fef5933bb18b1e4b25fc833ed934
Egor Lashkevich <hail.to.the.larva@gmail.com>
2018-03-22 21:42:47 +0300

    init

---
+++ EssayStorage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EssayStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
